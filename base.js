10f91407dcda44b1925b5eaf5906b0005f54014b
Yui Guji <nikitunzod@gmail.com>
2022-07-28 20:34:22 +0300

    Adding square divs & css styling

---
+++ base.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container')
+
+
+for (let i = 0; i < 256; i++) {
+
+   const square = document.createElement('div')
+   square.className = 'square'
+   
+   container.appendChild(square)   
+}
