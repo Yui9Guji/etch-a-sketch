a8c411ef18c05d874eb6c5b41c7bd965a3782931
Yui Guji <nikitunzod@gmail.com>
2022-07-28 21:12:32 +0300

    Adding buttons & changing grid

--- base.js
+++ base.js
@@ -12,7 +12,7 @@ const addingDivs = (amount) => {
    
 }
 
-container.style.width = '288px'
+container.style.width = '384px'
 
 addingDivs(256) 
 // amount of squares ** 2
@@ -25,6 +25,6 @@ const squares = document.querySelectorAll('.square')
 squares.forEach((square) => {
 
    square.addEventListener('mouseover', () => {
-      square.style.backgroundColor = 'red'
+      square.style.backgroundColor = 'black'
    })
 })
